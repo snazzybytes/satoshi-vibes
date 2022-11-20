3365df628dc437e2c702119298479ef50e67ce06
SnazzyBytes <snazzybytes@gmail.com>
2022-11-20 12:58:29 -0600

    enable vercel-analytics(beta)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import type { AppProps } from "next/app"
+import { Analytics } from "@vercel/analytics/react"
 import "@/styles/global.css"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
 
 // Manually disable console.log() in production (simpler/arguably more elegant than libs)
