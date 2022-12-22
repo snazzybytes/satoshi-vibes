a90d3399ace90d2939431e9df17b0e1632e76ed5
SnazzyBytes <snazzybytes@gmail.com>
2022-12-21 22:58:13 -0600

    add nextjs recommended config

--- next.config.js
+++ next.config.js
@@ -35,11 +35,17 @@ module.exports = {
   async headers() {
     return [
       {
-        // matching all NOSTR related routes
-        source: "/.well-known/nostr.json",
+        // matching all NOSTR nip5 (".well-known") related routes
+        source: "/.well-known/:path*",
         headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET" }
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
         ],
       },
     ]
