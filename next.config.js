2fc1a7a26cdc5b5b6883e7e4d36faa796b8f03a1
SnazzyBytes <snazzybytes@gmail.com>
2022-12-21 22:31:37 -0600

    remove excess headers

--- next.config.js
+++ next.config.js
@@ -35,17 +35,11 @@ module.exports = {
   async headers() {
     return [
       {
-        // matching all API routes
+        // matching all NOSTR related routes
         source: "/.well-known/nostr.json",
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
+          { key: "Access-Control-Allow-Methods", value: "GET" }
         ],
       },
     ]
