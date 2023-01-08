ba322cd965941141e4bd5580f15d55a84d76f6e1
Luke <72365181+snazzybytes@users.noreply.github.com>
2023-01-08 15:35:33 -0600

    remove excess headers (#28)

--- next.config.js
+++ next.config.js
@@ -38,13 +38,12 @@ module.exports = {
         // matching all NOSTR nip5 (".well-known") related routes
         source: "/.well-known/:path*",
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Methods", value: "GET" },
           {
             key: "Access-Control-Allow-Headers",
             value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+              "Origin, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
           },
         ],
       },
