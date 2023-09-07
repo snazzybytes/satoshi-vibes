7760fefbc75a859808f2697e5e290f0f256bced4
LukeOnchain <72365181+snazzybytes@users.noreply.github.com>
2023-09-07 16:48:15 -0500

    inaccurate (#54)

--- pages/about.tsx
+++ pages/about.tsx
@@ -44,30 +44,9 @@ const About = () => {
         <div
           className={`${styles.aboutPanel} ${utilStyles.breakWithHyphens} ${utilStyles.boxShadow}`}
         >
-          <h2 className={utilStyles.headingLg}>Motivation</h2>
-          <p>
-            I quickly put this site together while sitting home one weekend and exploring{" "}
-            <Link href="https://nextjs.org/">NextJS</Link>. I work as a software engineer in
-            e-commerce world dealing with backend systems in large enteprise setting and don't get
-            to play with the frontend dev that much. However I felt compelled to provide some
-            educational value to the world while I'm at it. There is always a new wave of bitcoin
-            plebs awakening, ready to step inside the mind of Satoshi once they truly realize that
-            something is wrong with the fiat world we live in 💸🪦. The least I could do is pay
-            homage to Bitcoin and spread its ethos out into the universe (inspired by{" "}
-            <a href="https://nakamotoinstitute.org/" target="_blank">
-              NakamototInstitute.org
-            </a>
-            ).
-          </p>
-        </div>
-
-        <div
-          className={`${styles.aboutPanel} ${utilStyles.breakWithHyphens} ${utilStyles.boxShadow}`}
-        >
           <h2 className={utilStyles.headingLg}>Contribute</h2>
           <p>
-            The site is fully open source, just like everything I used to put it together (credits
-            on Github). If you are a dev and want to add more pages, Bitcoin resources or learning
+            If you are a dev and want to add more pages, Bitcoin resources or learning
             materials, improve the NextJS layout or react components, or just spice things up and
             make this site a lot cooler for all the plebs - you are very welcome to{" "}
             <a href="https://github.com/snazzybytes/satoshi-vibes" target="_blank">
