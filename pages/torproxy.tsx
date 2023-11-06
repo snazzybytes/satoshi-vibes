ad07e77f588310bf370e746e37e86e7b4d3b79f4
SnazzyBytes <snazzybytes@gmail.com>
2023-11-06 16:23:08 -0600

    add Snowflake Tor Proxy to help the world

---
+++ pages/torproxy.tsx
@@ -0,0 +1,22 @@
+import utilStyles from "@/styles/utils.module.css"
+import styles from "@/styles/torproxy.module.css"
+import Layout from "@/components/layout"
+import Image from "next/image"
+
+const TorProxy = () => {
+  return (
+    <Layout homeButton>
+      <div className={`${styles.torPanel} ${utilStyles.breakWithHyphens} ${utilStyles.boxShadow}`}>
+        <h1 className={utilStyles.headingLg}>Snowflake ❄️ Tor Proxy</h1>
+        <p>Help people from all over the world to access censored websites and applications.</p>
+        <p>
+          If you switch on the Snowflake below and leave the browser tab open, a user can connect
+          through your new proxy!
+        </p>
+        <iframe src="https://snowflake.torproject.org/embed.html" width="320" height="240"></iframe>
+      </div>
+    </Layout>
+  )
+}
+
+export default TorProxy
