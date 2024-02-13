cdc3fb776a9d27846c0b3afc3a1e850a9fda8af6
LukeOnchain <72365181+snazzybytes@users.noreply.github.com>
2024-02-13 17:35:32 -0600

    add vercel speed-insights; update deps via npm audit (#60)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app"
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "@/styles/global.css"
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -7,6 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </>
   )
 }
